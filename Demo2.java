e3e5d37ca3f9bb210b4df11d59272072c07a0478
Hari Prasath <126060166+ihariprasath@users.noreply.github.com>
2026-01-07 12:06:21 +0530

    Update Demo2.java

--- Demo2.java
+++ Demo2.java
@@ -1,4 +1,4 @@
 class Demo2{
 	public static void main(String[] args){
-		System.out.println("Welcome to Java Project connecting to github")
+		System.out.println("Welcome to Java Project connecting to github");
 }
